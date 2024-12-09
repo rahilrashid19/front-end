ff892248a93d09db1b02cbb9ac83008f018944f1
Rahil Rashid <rrashid@sparkcognition.com>
2024-12-09 11:30:59 +0530

    Changes - Added Edit Profile Functionality - updating the profile on save of edit profile component

---
+++ src/components/EditProfile.tsx
@@ -0,0 +1,148 @@
+import { Button } from "./ui/button";
+import { useForm } from "react-hook-form";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "./ui/input";
+import axios from "axios";
+import { BASE_URL } from "@/utils/constants";
+import { ToastAction } from "@/components/ui/toast";
+import { useToast } from "@/hooks/use-toast";
+import { useDispatch, UseDispatch } from "react-redux";
+import { addUser, removeUser } from "@/utils/slices/userSlice";
+
+interface EditProfileProps {
+  onClose: () => void;
+  user: User;
+}
+
+interface User {
+  firstName: string;
+  lastName: string;
+  bio: string;
+  profilePic: string;
+  age: number;
+}
+
+const EditProfile = ({ onClose, user }: EditProfileProps) => {
+  const dispatch = useDispatch();
+  const { toast } = useToast();
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      bio: user.bio,
+      profilePic: user.profilePic,
+      age: user.age,
+    },
+  });
+
+  const submitUpdatedForm = async (data: User) => {
+    const respose = await axios.patch(
+      BASE_URL + "editProfile",
+      { ...data },
+      {
+        withCredentials: true,
+      }
+    );
+    if (respose.status === 202) {
+      dispatch(removeUser());
+      dispatch(addUser(respose.data.loggedInUser));
+      toast({
+        variant: "default",
+        title: "User Updated Successfully",
+        action: <ToastAction altText="Okay">Okay !</ToastAction>,
+      });
+      onClose();
+    } else {
+      toast({
+        variant: "destructive",
+        title: "Failed to update user",
+        action: <ToastAction altText="Okay">Okay !</ToastAction>,
+      });
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        {user && (
+          <div className="min-h-screen flex items-center justify-center">
+            <Card className="w-full max-w-sm mx-auto shadow-lg rounded-lg p-6 bg-white">
+              <Button onClick={onClose}>Go Back</Button>
+              <CardHeader>
+                <CardTitle className="text-2xl font-bold text-center text-gray-800 mb-4">
+                  Edit Profile
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <form noValidate onSubmit={handleSubmit(submitUpdatedForm)}>
+                  <div className="max-w-sm mx-auto p-6  space-y-4">
+                    <div>
+                      <Label className="block text-sm font-medium text-gray-700">
+                        First Name
+                      </Label>
+                      <Input
+                        type="text"
+                        placeholder="First Name"
+                        {...register("firstName")}
+                        className="mt-1 w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                    <div>
+                      <Label className="block text-sm font-medium text-gray-700">
+                        Last Name
+                      </Label>
+                      <Input
+                        type="text"
+                        placeholder="Last Name"
+                        {...register("lastName")}
+                        className="mt-1 w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                    <div>
+                      <Label className="block text-sm font-medium text-gray-700">
+                        Age
+                      </Label>
+                      <Input
+                        type="number"
+                        placeholder="Age"
+                        {...register("age")}
+                        className="mt-1 w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                    <div>
+                      <Label className="block text-sm font-medium text-gray-700">
+                        Bio
+                      </Label>
+                      <Input
+                        placeholder="Bio"
+                        {...register("bio")}
+                        className="mt-1 w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      ></Input>
+                    </div>
+                    <div>
+                      <Label className="block text-sm font-medium text-gray-700">
+                        Profile Picture
+                      </Label>
+                      <Input
+                        type="url"
+                        placeholder="Profile Picture URL"
+                        {...register("profilePic")}
+                        className="mt-1 w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                    <Button type="submit" className="w-full py-2 px-4 ">
+                      Submit
+                    </Button>
+                  </div>
+                </form>
+              </CardContent>
+            </Card>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default EditProfile;
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,41 +1,63 @@
 import { useSelector } from "react-redux";
 import { RootState } from "@/utils/store/store";
-
+import { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "./ui/button";
+import EditProfile from "./EditProfile";
 export const Profile = () => {
   const data = useSelector((state: RootState) => state.user);
+  const [isEditble, setIsEditble] = useState(false);
+  const showEditProfile = () => {
+    setIsEditble(true);
+  };
+
+  const onClose = () => {
+    setIsEditble(false);
+  };
+
   return (
     <div>
-      {data.user[0] && (
-        <div className="min-h-screen flex items-center justify-center">
-          <Card className="w-full max-w-sm mx-auto shadow-lg rounded-lg p-6 bg-white">
-            <CardHeader>
-              <CardTitle className="text-2xl font-bold text-center text-gray-800 mb-4">
-                Profile
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="max-w-sm mx-auto p-6">
-                <img
-                  src={data.user[0].profilePic}
-                  alt="profile-picture"
-                  className="w-24 h-24 mx-auto rounded-full border-2 border-indigo-500 object-cover mb-4"
-                />
-                <div className="text-center">
-                  <p className="text-xl font-semibold text-gray-800">
-                    {data.user[0].firstName + " " + data.user[0].lastName}
-                  </p>
-                  <p className="text-gray-600">
-                    {data.user[0].age + " years old, " + data.user[0].gender}
-                  </p>
-                  <p className="text-gray-800 font-semibold mt-3">
-                    {data.user[0].bio}
-                  </p>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
+      {!isEditble ? (
+        <div>
+          {data.user[0] && (
+            <div className="min-h-screen flex items-center justify-center">
+              <Card className="w-full max-w-sm mx-auto shadow-lg rounded-lg p-6 bg-white">
+                <Button className="flex float-end" onClick={showEditProfile}>
+                  Edit Profile
+                </Button>
+                <CardHeader>
+                  <CardTitle className="text-2xl font-bold text-center text-gray-800 mb-4">
+                    Profile
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="max-w-sm mx-auto p-6">
+                    <img
+                      src={data.user[0].profilePic}
+                      alt="profile-picture"
+                      className="w-24 h-24 mx-auto rounded-full border-2 border-indigo-500 object-cover mb-4"
+                    />
+                    <div className="text-center">
+                      <p className="text-xl font-semibold text-gray-800">
+                        {data.user[0].firstName + " " + data.user[0].lastName}
+                      </p>
+                      <p className="text-gray-600">
+                        {data.user[0].age +
+                          " years old, " +
+                          data.user[0].gender}
+                      </p>
+                      <p className="text-gray-800 font-semibold mt-3">
+                        {data.user[0].bio}
+                      </p>
+                    </div>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+          )}
         </div>
+      ) : (
+        <EditProfile onClose={onClose} user={data.user[0]} />
       )}
     </div>
   );
